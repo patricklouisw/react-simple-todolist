8cd8779f0d26d9e199d56e2723e4d55d78e8543e
Patrick Louis <patricklouisw@gmail.com>
2023-05-18 15:13:24 -0400

    Upload todo list project

---
+++ src/TodoItem.jsx
@@ -0,0 +1,22 @@
+export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        {title}
+      </label>
+      <button
+        className="btn btn-danger"
+        onClick={(e) => {
+          deleteTodo(id);
+        }}
+      >
+        Delete
+      </button>
+    </li>
+  );
+}
